12ccbc96e5f51cac14540a4a474a0821b3b35eec
Xin Kai <tam.xin.kai@gmail.com>
2022-04-15 16:35:27 +0800

    daily update
    added API call to old heroku
    clean up styling

--- screens/properties/PropertiesScreen.js
+++ screens/properties/PropertiesScreen.js
@@ -4,47 +4,62 @@ import { FontAwesome } from "@expo/vector-icons";
 import Colors from "../../constants/colors";
 import { createStackNavigator } from "@react-navigation/stack";
 import SpecificPropertiesScreen from "./SpecificPropertyScreen";
-import data from './data.js';
-
+import API from './API.js'
+import {useState} from 'react'
+import customStyles from "./propertyStyles.js";
+//todo agent details
 
 const PropertyHome = ({navigation})=>{
-  const content = data.map(obj=>{
-    return(
-      <View key={obj.id}> 
-        <Pressable onPress={()=>navigation.navigate(`SpecificProperty`, {props: obj})}> 
-        <Image style={customStyles.image} source={require('../../assets/images/property-images/Sky-Vue-Ang-Mo-Kio-Bishan-Thomson-Singapore-1.jpg')}></Image>
-          <View style={customStyles.borderNoTop}>
-          <Text style={[textStyles.bodyText, customStyles.textPadding]}>{obj.propertyName}</Text>
-          <Text style={[textStyles.bodyText, customStyles.smallFont]}>{obj.address}</Text>
-          <View style={customStyles.horizontal}>
-            {obj.saleType === 'rent'? <Text style={[textStyles.bodyText, customStyles.textPadding ]}>{obj.price} /mo</Text>:<Text style={[textStyles.bodyText, customStyles.textPadding ]}>{obj.price}</Text>}
-            <Text style={[textStyles.bodyText, customStyles.smallFont]}>Availability</Text>
-          </View>
-          <View style={customStyles.horizontal}>
-            <Text style={[textStyles.bodyText, customStyles.smallFont]}>{obj.noOfBedrooms} <FontAwesome name='bed' /></Text>
-            <Text style={[textStyles.bodyText, customStyles.smallFont]}>{obj.noOfBaths} <FontAwesome name='bathtub' /></Text>
-            <Text style={[textStyles.bodyText, customStyles.smallFont]}>{obj.floorsize} sqft</Text>
-            <Text style={[textStyles.bodyText, customStyles.smallFont]}>S$ {obj.pricePSF} psf</Text>
-          </View>
-            <Text style={[textStyles.bodyText, customStyles.smallFont]}>distance to MRT</Text>
-          </View>
-          <View style={[customStyles.borderNoTop,]}>
-            <Text style={[textStyles.bodyText, customStyles.smallFont]}>Agent memo</Text>
-            <Text style={[textStyles.bodyText, customStyles.smallFont]}>{obj.User.firstName}{obj.User.lastName}</Text>
-            <View style={[customStyles.justifyContainerMid]}>
-              <Pressable style={[customStyles.widthHalf]}><Text style={[textStyles.bodyText, customStyles.contactButton]}>Whatsapp</Text></Pressable>
-              <Pressable style={[customStyles.widthHalf]}><Text style={[textStyles.bodyText, customStyles.contactButton]}>Call</Text></Pressable>
-            </View>
-          </View>
-        </Pressable>
-      </View>
-    ) 
-  })
-  return (
+
+  const [content, setContent]=useState([]);
+
+  (async()=>{
+    const incoming = await API.get('/properties');
+    const properties = incoming.data.data;
+    setContent(properties);
+  })();
+
+  return(
     <ScrollView >
       <View style={styles.container}>
-        <Text>{data.length} residential properties for rent</Text>
-          {content}
+        <Text style={customStyles.textContainer}>{content.length} residential properties for rent</Text>
+          {content.map(obj=>{
+            return(
+              <View key={obj.id}> 
+                <ScrollView pagingEnabled={true} horizontal={true} showsHorizontalScrollIndicator={false} style={customStyles.imageContainer}>
+                  <Image style={customStyles.image} source={require('../../assets/images/property-images/Sky-Vue-Ang-Mo-Kio-Bishan-Thomson-Singapore-1.jpg')}></Image>
+                  <Image style={customStyles.image} source={require('../../assets/images/property-images/Sky-Vue-Ang-Mo-Kio-Bishan-Thomson-Singapore-2.jpg')}></Image>
+                  <Image style={customStyles.image} source={require('../../assets/images/property-images/Sky-Vue-Ang-Mo-Kio-Bishan-Thomson-Singapore-3.jpg')}></Image>
+                </ScrollView>
+                <Pressable onPress={()=>navigation.navigate(`SpecificProperty`, {props: obj})}> 
+                  <View style={customStyles.borderNoTop}>
+                  <Text style={[textStyles.bodyText, customStyles.textPadding,customStyles.textContainer]}>{obj.propertyName}</Text>
+                  <Text style={[textStyles.bodyText, customStyles.fontSmall,customStyles.textContainer]}>{obj.address}</Text>
+                  <View style={customStyles.horizontal}>
+                    {obj.saleType === 'rent'? 
+                      <Text style={[textStyles.bodyText, customStyles.textPadding,customStyles.textContainer,]}>{obj.price} /mo</Text>
+                      :<Text style={[textStyles.bodyText, customStyles.textPadding, customStyles.textContainer ]}>{obj.price}</Text>}
+                    <Text style={[textStyles.bodyText, customStyles.fontSmall]}>Availability</Text>
+                  </View>
+                  <View style={[customStyles.horizontal,customStyles.textContainer]}>
+                    <Text style={[textStyles.bodyText, customStyles.fontSmall]}>{obj.noOfBedrooms} <FontAwesome name='bed' /></Text>
+                    <Text style={[textStyles.bodyText, customStyles.fontSmall]}>{obj.noOfBaths} <FontAwesome name='bathtub' /></Text>
+                    <Text style={[textStyles.bodyText, customStyles.fontSmall]}>{obj.floorsize} sqft</Text>
+                    <Text style={[textStyles.bodyText, customStyles.fontSmall]}>S$ {obj.pricePSF} psf</Text>
+                  </View>
+                    <Text style={[textStyles.bodyText, customStyles.fontSmall, customStyles.textContainer]}>distance to MRT</Text>
+                  </View>
+                  <View style={[customStyles.borderNoTop, customStyles.textContainer]}>
+                    <Text style={[textStyles.bodyText, customStyles.fontSmall]}>Agent memo</Text>
+                    <Text style={[textStyles.bodyText, customStyles.fontSmall]}>{obj.User.firstName}{obj.User.lastName}</Text>
+                    <View style={[customStyles.justifyContainerMid]}>
+                      <Pressable style={[customStyles.widthHalf]}><Text style={[textStyles.bodyText, customStyles.contactButton]}>Whatsapp</Text></Pressable>
+                      <Pressable style={[customStyles.widthHalf]}><Text style={[textStyles.bodyText, customStyles.contactButton]}>Call</Text></Pressable>
+                    </View>
+                  </View>
+                </Pressable>
+              </View>
+            )})}
       </View>
     </ScrollView>
   )
@@ -58,8 +73,6 @@ const FilterButtons = ()=>{
 
 
 function PropertiesScreen() {
-  //todo API call by default, need add API call to fill in all the texts below
-  //todo image scrolling
   const Stack = createStackNavigator();
 
   return (
@@ -82,47 +95,3 @@ function PropertiesScreen() {
 
 export default PropertiesScreen;
 
-const customStyles = StyleSheet.create({
-  image:{
-    width: '100%',
-    height: undefined,
-    aspectRatio: 1.3,
-    marginTop: 10,
-  },
-  horizontal:{
-    display: 'flex',
-    flexDirection: "row",
-    alignItems: 'flex-end',
-  },
-  textPadding:{
-    paddingHorizontal: 10,
-  },
-  smallFont:{
-    fontSize: 14,
-    paddingHorizontal: 10,
-  },
-  borderNoTop:{
-    borderBottomWidth: 1,
-    paddingVertical: 10,
-    width: '100%'
-  },
-  justifyContainerMid:{
-    display: 'flex',
-    flexDirection: "row",
-    width: '100%',
-    padding: 10,
-  },
-  contactButton:{
-    width: '90%',
-    alignSelf: 'center',
-    fontSize: 15,
-    paddingVertical: 5,
-    textAlign: 'center',
-    borderWidth: 1,
-    borderRadius: 10,
-  },
-  widthHalf:{
-    width: '50%',
-    justifyContent: 'center',
-  },
-});
--- screens/properties/SpecificPropertyScreen.js
+++ screens/properties/SpecificPropertyScreen.js
@@ -2,46 +2,50 @@ import { View, Text, StyleSheet, Image, ScrollView, Pressable } from "react-nati
 import { styles, textStyles } from "../../styles/common";
 import { FontAwesome } from "@expo/vector-icons";
 import Colors from "../../constants/colors";
+import customStyles from "./propertyStyles.js";
+//todo agent details
 
 function SpecificPropertiesScreen({route}) {
     let data = route.params.props;
     let tempDate = new Date(route.params.props.createdAt).toDateString();
     let date = `${tempDate.slice(4,10)}, ${tempDate.slice(11)}`
-    //todo API call by default, need add API call to fill in all the texts below
-    //todo image scrolling
     return (
         <ScrollView >
-            <View style={styles.container}>
-                <View style={customStyles.borderBottomWidth}>
-                    <Text style={customStyles.fontBig}>{data.propertyName}</Text>
-                    <Image style={customStyles.image} source={require('../../assets/images/property-images/Sky-Vue-Ang-Mo-Kio-Bishan-Thomson-Singapore-1.jpg')}></Image>
-                    <Text style={customStyles.fontBig}>{data.price} /mo</Text>
-                    <View style={customStyles.horizontal}>
+            <View style={[styles.container, {width:'100%'}]}>
+                <View style={[customStyles.borderBottomWidth, {width:'100%'}]}>
+                    <Text style={[customStyles.fontBig, customStyles.textContainer]}>{data.propertyName}</Text>
+                    <ScrollView pagingEnabled={true} horizontal={true} showsHorizontalScrollIndicator={false} style={customStyles.imageContainer}>
+                        <Image style={customStyles.image} source={require('../../assets/images/property-images/Sky-Vue-Ang-Mo-Kio-Bishan-Thomson-Singapore-1.jpg')}></Image>
+                        <Image style={customStyles.image} source={require('../../assets/images/property-images/Sky-Vue-Ang-Mo-Kio-Bishan-Thomson-Singapore-2.jpg')}></Image>
+                        <Image style={customStyles.image} source={require('../../assets/images/property-images/Sky-Vue-Ang-Mo-Kio-Bishan-Thomson-Singapore-3.jpg')}></Image>
+                    </ScrollView>
+                    <Text style={[customStyles.fontBig, customStyles.textContainer]}>{data.price} /mo</Text>
+                    <View style={[customStyles.horizontal, customStyles.textContainer]}>
                         <Text style={[textStyles.bodyText, customStyles.fontSmall]}>{data.noOfBedrooms} <FontAwesome name='bed' /></Text>
                         <Text style={[textStyles.bodyText, customStyles.fontSmall]}>{data.noOfBaths} <FontAwesome name='bathtub' /></Text>
                         <Text style={[textStyles.bodyText, customStyles.fontSmall]}>{data.floorsize} sqft</Text>
                         <Text style={[textStyles.bodyText, customStyles.fontSmall]}>S$ {data.pricePSF} psf</Text>
                     </View>
                     <Text></Text>
-                    <Text style={[textStyles.bodyText, customStyles.textPadding]}>{data.propertyName}</Text>
-                    <Text style={[textStyles.bodyText, customStyles.fontSmall]}>{data.address}</Text>
-                    <Text style={[textStyles.bodyText, customStyles.fontSmall]}>Distance to MRT</Text>
+                    <Text style={[textStyles.bodyText, customStyles.textPadding, customStyles.textContainer]}>{data.propertyName}</Text>
+                    <Text style={[textStyles.bodyText, customStyles.fontSmall,customStyles.textContainer]}>{data.address}</Text>
+                    <Text style={[textStyles.bodyText, customStyles.fontSmall,customStyles.textContainer]}>Distance to MRT</Text>
                 </View>
-                <View style={[customStyles.borderBottomWidth, customStyles.horizontalDetails]}>
-                    <View style={customStyles.widthHalf}>
-                        <Text style={customStyles.details}>Type</Text>
-                        <Text style={customStyles.details}>Size</Text>
-                        <Text style={customStyles.details}>Built</Text>
-                        <Text style={customStyles.details}>Move-in Date</Text>
-                        <Text style={customStyles.details}>Listing Type</Text>
-                        <Text style={customStyles.details}>Furnishing</Text>
-                        <Text style={customStyles.details}>Tenure</Text>
-                        <Text style={customStyles.details}>Developer</Text>
-                        <Text style={customStyles.details}>Sub Type</Text>
-                        <Text style={customStyles.details}>Listing id</Text>
-                        <Text style={customStyles.details}>Listed</Text>
+                <View style={[customStyles.borderBottomWidth, customStyles.horizontal]}>
+                    <View style={[customStyles.widthHalf, customStyles.textContainer]}>
+                        <Text style={customStyles.detailsFont}>Type</Text>
+                        <Text style={customStyles.detailsFont}>Size</Text>
+                        <Text style={customStyles.detailsFont}>Built</Text>
+                        <Text style={customStyles.detailsFont}>Move-in Date</Text>
+                        <Text style={customStyles.detailsFont}>Listing Type</Text>
+                        <Text style={customStyles.detailsFont}>Furnishing</Text>
+                        <Text style={customStyles.detailsFont}>Tenure</Text>
+                        <Text style={customStyles.detailsFont}>Developer</Text>
+                        <Text style={customStyles.detailsFont}>Sub Type</Text>
+                        <Text style={customStyles.detailsFont}>Listing id</Text>
+                        <Text style={customStyles.detailsFont}>Listed</Text>
                     </View>
-                    <View style={{width:'50%'}}> 
+                    <View style={[customStyles.widthHalf, customStyles.textContainer]}> 
                         <Text style={customStyles.details}>{data.propertyType}</Text>
                         <Text style={customStyles.details}>{data.floorsize} sqft</Text>
                         <Text style={customStyles.details}>{data.TOPYear}</Text>
@@ -59,9 +63,9 @@ function SpecificPropertiesScreen({route}) {
             <View style={styles.container}>
                 <Text style={[textStyles.bodyText, customStyles.textPadding, customStyles.fontSize]}>{data.User.firstName}{data.User.lastName}</Text>
                 <View style={[customStyles.justifyContainerMid]}>
-                    <Pressable style={[customStyles.width13]}><Text style={[textStyles.bodyText, customStyles.contactButton]}>Whatsapp</Text></Pressable>
-                    <Pressable style={[customStyles.width13]}><Text style={[textStyles.bodyText, customStyles.contactButton]}>SMS</Text></Pressable>
-                    <Pressable style={[customStyles.width13]}><Text style={[textStyles.bodyText, customStyles.contactButton]}>Call</Text></Pressable>
+                    <Pressable style={[customStyles.width13]}><Text style={[textStyles.bodyText, customStyles.contactButtonSpecificProperty]}>Whatsapp</Text></Pressable>
+                    <Pressable style={[customStyles.width13]}><Text style={[textStyles.bodyText, customStyles.contactButtonSpecificProperty]}>SMS</Text></Pressable>
+                    <Pressable style={[customStyles.width13]}><Text style={[textStyles.bodyText, customStyles.contactButtonSpecificProperty]}>Call</Text></Pressable>
                 </View>
             </View>
         </ScrollView>
@@ -69,65 +73,3 @@ function SpecificPropertiesScreen({route}) {
 }
 
 export default SpecificPropertiesScreen;                                  
-
-const customStyles = StyleSheet.create({
-    image:{
-        width: '100%',
-        height: undefined,
-        aspectRatio: 1.3,
-    },
-    horizontal:{
-        display: 'flex',
-        flexDirection: "row",
-        alignItems: 'flex-end',
-        flex: 1,
-    },
-    horizontalDetails:{
-        display: 'flex',
-        flexDirection: "row",
-        alignItems: 'flex-end',
-    },
-    details:{
-        fontSize: 13,
-    },
-    textPadding:{
-        paddingRight: 10,
-    },
-    fontSize:{
-        fontSize: 15,
-    },
-    fontBig:{
-        fontSize: 24,
-        paddingRight: 10,
-    },
-    fontSmall:{
-        fontSize: 15,
-        paddingRight: 10,
-    },
-    borderBottomWidth:{
-        paddingVertical: 10,
-        borderBottomWidth: 1,
-    },
-    widthHalf:{
-        width: '50%',
-    },  
-    width13:{
-        width: '33%',
-    },  
-    contactButton:{
-        width: '90%',
-        alignSelf: 'center',
-        fontSize: 13,
-        paddingVertical: 5,
-        textAlign: 'center',
-        borderWidth: 1,
-        borderRadius: 10,
-    },
-    justifyContainerMid:{
-        display: 'flex',
-        flexDirection: "row",
-        width: '100%',
-        padding: 10,
-    },
-
-});
