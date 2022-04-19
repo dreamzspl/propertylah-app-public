2e425eb29211b8eef3453e77af7ced97c884454c
Juzantino Junadi <juzantinojunadi@Juzantinos-MacBook-Pro.local>
2022-04-19 12:19:39 +0800

    updated QNAQuesAnswer screen with form pop up

--- screens/qna/QnAQnsAnswer.js
+++ screens/qna/QnAQnsAnswer.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import { View, Text ,ScrollView, StyleSheet, Alert} from 'react-native'
-import PrimaryButton from '../../components/UI/PrimaryButton'
 
+import { View, Text ,ScrollView, StyleSheet, Alert, Pressable} from 'react-native'
+import PrimaryButton from '../../components/UI/PrimaryButton'
+import ModalPopUp from "../../components/UI/ModalPopUp";
+import React, { useState } from "react";
+import CustomInput from "../../components/UI/CustomInput";
+import { Ionicons } from "@expo/vector-icons";
+import { useForm, Controller } from "react-hook-form";
+import { useNavigation } from '@react-navigation/native';
+import { styles, textStyles } from "../../styles/common";
+import customStyles from "./QnAStyles";
 
 const AnswerHeader = () => {
     return (
-        <View style={styles.headerContainer}>
-            <View style={styles.ansInput}>
+        <View style={customstyles.headerContainer}>
+            <View style={customstyles.ansInput}>
             <Text style={{fontSize: 13}}>AskGuru Q&A Community</Text>
             <Text style={{ fontSize: 20 }}>Get answers from PropertyGuru Experts</Text>
             <Text style={{ color: 'red', fontSize: 20 }}>in 24 Hours</Text>  
@@ -22,7 +29,7 @@ const QuestionList = ({ category, name, date, questionAsked, views }) => {
         Alert.alert('Onpress called')
     }
     return (
-            <View style={styles.questionContainer}>
+            <View style={customstyles.questionContainer}>
                 <Text style={{color:'red', fontSize:14}}  onPress={showMessage}>{category}</Text>
                 <Text>Asked by {name} on {date}</Text>
                 <Text>{questionAsked}</Text>
@@ -34,7 +41,7 @@ const QuestionList = ({ category, name, date, questionAsked, views }) => {
 //show the list of answers link as foreign key to question from the api
 const AnswerList = ({name, date , answerInput}) => {
     return (
-        <View style={styles.answerContainer}>
+        <View style={customstyles.answerContainer}>
             <Text >{name}</Text>
             <Text>Replied {date}</Text>
             <Text>{answerInput}</Text>
@@ -46,6 +53,43 @@ const AnswerList = ({name, date , answerInput}) => {
 
 
 const QnAQnsAnswer = () => {
+    const [ loading, setLoading ] = useState(false);
+    const [isModalVisible, setIsModalVisible] = useState(false);
+  
+    const handleClose = () => setIsModalVisible(false);
+
+    const onAskQnPressed = () => {
+    if (loading) {
+      return;
+    }
+    setLoading(true);
+    try {
+      setIsModalVisible(true);
+    } catch (e) {
+      Alert.alert("Oops", e.message);
+    }
+    console.warn("'Ask Your Question' button pressed");
+    setLoading(false);
+  }
+  // create form values
+  const { control, handleSubmit, formState: { errors } } = useForm({
+    defaultValues: {
+      question: '',
+      topic: '',
+      projName: '',
+      displayName: '',
+    }
+  });
+  // const onSubmit = data => console.log(data);
+
+  // 'Submit' button function
+  const onSubmitBtnPressed = (data) => {
+    console.log(data);
+    setIsModalVisible(false);
+    console.warn("'Submit' button pressed");
+  }
+
+
   return (
     <ScrollView>
           <AnswerHeader />
@@ -59,15 +103,38 @@ const QnAQnsAnswer = () => {
               name='Ryan'
               date='13 Apr 2022'
               answerInput='This is the answer for the question' />
-          <View style={styles.primaryButton}>
-                <PrimaryButton text= "Ask Your Question" onPress={'onAskQnPressed'} type="Primary"/>
+          <View style={customstyles.primaryButton}>
+                <PrimaryButton text= "Ask Your Question" onPress={onAskQnPressed} type="Primary"/>
           </View>
+          <ModalPopUp visible={isModalVisible}>
           
+          <Pressable onPress={handleClose}>
+            <Text style={{ alignSelf: "flex-end", paddingRight: 10, paddingTop: 10 }}>
+              <Ionicons style={customStyles.icon} name="close-circle-outline"/>
+            </Text> 
+          </Pressable>
+          <View style={customStyles.formContainer}>
+            <Text style={[textStyles.headerText, { flexWrap: "wrap" }]}>Ask Your Question</Text>
+            <Text style={[textStyles.bodyText, { marginVertical: 10 }]}>Our PropertyLah experts will answer you within 24 hours! 😎</Text>
+            <View>
+              <CustomInput label="Question" name="question" control={control} rules={{required: 'Please ask a question 😊'}} placeholder="Start with 'How', 'What', 'Where', 'Why', etc" numberOfLines={5} />
+
+              <CustomInput label="Your email" name="email" control={control} rules={{required: 'Please provide a valid email address.'}} placeholder="email@example.com" numberOfLines={1} />
+
+              <CustomInput label="Project Name" name="projName" control={control} placeholder="8 Riversuites" numberOfLines={1} />
+
+              <CustomInput label="Your name" name="displayName" control={control} placeholder="e.g. Jason Teo" numberOfLines={1} />
+
+              <PrimaryButton text="Submit" onPress={handleSubmit(onSubmitBtnPressed)} />
+
+            </View>
+          </View>
+        </ModalPopUp>
     </ScrollView>
   )
 }
 
-const styles = StyleSheet.create({
+const customstyles = StyleSheet.create({
     container: {
         flex: 1
     },
