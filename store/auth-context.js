363eaf5e4dd6e6c1c2b27464d3578012dd5a933e
thinkdaniel <danielgoh@outlook.com>
2022-04-20 17:04:15 +0800

    completed authentication

--- store/auth-context.js
+++ store/auth-context.js
@@ -1,4 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { set } from "react-native-reanimated";
 
 const defaultAuthDetails = {
   id: "",
@@ -20,12 +22,31 @@ export const AuthContext = createContext({
 export default function AuthContextProvider({ children }) {
   const [authDetails, setAuthDetails] = useState(defaultAuthDetails);
 
+  useEffect(() => {
+    async function fetchAuthDetails() {
+      const storedAuthDetails = JSON.parse(
+        await AsyncStorage.getItem("authDetails")
+      );
+
+      if (storedAuthDetails) {
+        setAuthDetails(storedAuthDetails);
+      }
+    }
+
+    fetchAuthDetails();
+  }, []);
+
   function authenticate(id, firstName, role, token) {
     setAuthDetails({ id, firstName, role, token });
+    AsyncStorage.setItem(
+      "authDetails",
+      JSON.stringify({ id, firstName, role, token })
+    );
   }
 
   function logout() {
     setAuthDetails(defaultAuthDetails);
+    AsyncStorage.removeItem("authDetails");
   }
 
   const contextValue = {
